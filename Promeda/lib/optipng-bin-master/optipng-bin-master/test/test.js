d70230244261a306a1dd117440740a38923bb853
TROEDER <t.roeder@promondo.de>
2019-01-25 14:56:44 +0100

    added img compressor jpegtran.exe, jpegoptim.exe, cjpeg.exe, usw.

---
+++ Promeda/lib/optipng-bin-master/optipng-bin-master/test/test.js
@@ -0,0 +1,50 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+const test = require('ava');
+const execa = require('execa');
+const tempy = require('tempy');
+const binCheck = require('bin-check');
+const binBuild = require('bin-build');
+const compareSize = require('compare-size');
+const optipng = require('..');
+
+test('rebuild the optipng binaries', async t => {
+	// Skip the test on Windows
+	if (process.platform === 'win32') {
+		t.pass();
+		return;
+	}
+
+	const tmp = tempy.directory();
+
+	await binBuild.url('http://downloads.sourceforge.net/project/optipng/OptiPNG/optipng-0.7.7/optipng-0.7.7.tar.gz', [
+		`./configure --with-system-zlib --prefix="${tmp}" --bindir="${tmp}"`,
+		'make install'
+	]);
+
+	t.true(fs.existsSync(path.join(tmp, 'optipng')));
+});
+
+test('return path to binary and verify that it is working', async t => {
+	t.true(await binCheck(optipng, ['--version']));
+});
+
+test('minify a PNG', async t => {
+	const tmp = tempy.directory();
+	const src = path.join(__dirname, 'fixtures/test.png');
+	const dest = path.join(tmp, 'test.png');
+	const args = [
+		'-strip',
+		'all',
+		'-clobber',
+		'-out',
+		dest,
+		src
+	];
+
+	await execa(optipng, args);
+	const res = await compareSize(src, dest);
+
+	t.true(res[dest] < res[src]);
+});
